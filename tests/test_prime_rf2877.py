26603f6d53d65b46f3c766a0c5cec37794d95c93
RF15R <rf2877@columbia.edu>
2023-10-29 19:39:38 -0400

    update

--- tests/test_prime_rf2877.py
+++ tests/test_prime_rf2877.py
@@ -1,1 +1,13 @@
 from prime_rf2877 import prime_rf2877
+from prime import is_prime
+
+def test_is_prime():
+    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    for prime in prime_numbers:
+        assert is_prime(prime) == True, f'Error: {prime} is not prime.'
+        print(f"{prime} is prime.")
+
+    composite_numbers = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20]
+    for composite in composite_numbers:
+        assert is_prime(composite) == False, f'Error: {composite} is not composite.'
+        print(f"{composite} is composite.")
